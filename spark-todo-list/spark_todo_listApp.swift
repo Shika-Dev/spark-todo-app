7ed927b422c3169da187d434ba8ce0c990ba9844
Gde Parama Artha Dharma <gededharma18@gmail.com>
2025-04-02 14:16:46 +0800

    Initial Commit

---
+++ spark-todo-list/spark_todo_listApp.swift
@@ -0,0 +1,17 @@
+//
+//  spark_todo_listApp.swift
+//  spark-todo-list
+//
+//  Created by Parama Artha on 02/04/25.
+//
+
+import SwiftUI
+
+@main
+struct spark_todo_listApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ spark-todo-listTests/spark_todo_listTests.swift
@@ -0,0 +1,17 @@
+//
+//  spark_todo_listTests.swift
+//  spark-todo-listTests
+//
+//  Created by Parama Artha on 02/04/25.
+//
+
+import Testing
+@testable import spark_todo_list
+
+struct spark_todo_listTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
